b0bcbcd274fd085b5406f01197dc3673e0d88210
Vivien <vivien.boucher@outlook.com>
2020-12-09 12:46:51 +0100

    Init fix template voter
    rm github pipelines

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/alice/voter
+
+go 1.15
+
+require (
+	github.com/cosmos/cosmos-sdk v0.39.1
+	github.com/cosmos/sdk-tutorials/voter/voter v0.0.0-20201208163316-750744a97115
+	github.com/google/uuid v1.0.0
+	github.com/gorilla/mux v1.7.4
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/viper v1.7.0
+	github.com/tendermint/go-amino v0.15.1
+	github.com/tendermint/tendermint v0.33.7
+	github.com/tendermint/tm-db v0.5.1
+)
---
+++ vue/src/views/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="sp-container">
+      <sp-sign-in />
+      <sp-bank-balances />
+      <sp-token-send />
+      <!-- this line is used by starport scaffolding # 4 -->
+		<sp-type-form type="poll" :fields="['title', 'options', ]" module="voter" />
+      <app-text type="h1">voter</app-text>
+      <wallet />
+      <poll-form />
+      <poll-list />
+    </div>
+  </div>
+</template>
+
+<script>
+import * as sp from "@tendermint/vue";
+
+
+export default {
+  components: { ...sp },
+};
+</script>
